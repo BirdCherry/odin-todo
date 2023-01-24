42eabf94783b1b4861fbdb99b736a95f90b5c4e6
BirdCherry <58888486+BirdCherry@users.noreply.github.com>
2023-01-24 21:20:39 +0200

    Make buttons and stuff work (visually)

--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
 import css from "./style.css";
 import { uiController } from "./modules/uiController.js";
 import { dataController } from "./modules/dataController.js";
-
+// TODO: add trash icon
+// TODO: add star icon (pin to top)
 
 
 const inputHandler = (() => {
     document.addEventListener('click', event => {
+        let element = '';
+        if (event.target.closest('.task-box')) {
+            element = 'task'
+        } else if (event.target.closest('li')) {
+            element = 'project'
+        } else if (event.target.closest('.new-task')) {
+            element = 'newTask'
+        }
+        uiController.setFocus(event, element);
+
+        // task item clicked: remove focus from all others, save data, set class .focused, update DOM?
+
+        // project list item clicked: set class .selected, remove class from all others, update DOM with filtered task view?
+
+        // new task button clicked, create new empty task, set it at the top, set it .focused, automatically add to currently selected project?
+
+
+
+        // if clicked on something else, remove focus from task items
+        // TODO: move ui stuff to uiController.js
+        // TODO: save contents from focused task item before removing focus
+        // TODO: if task is empty, discard it
+
         // Check if click was on a tasks item
         // TODO: move ui stuff to uiController.js
-        if (event.target.closest('.task-box')) {
-            let clickedTaskItem = event.target.closest('.task-box');
-            if (!clickedTaskItem.classList.contains('focused')) {
-                clickedTaskItem.classList.add('focused');
-            }
-        } else {
-            // if clicked on something else, remove focus from task items
-            // TODO: move ui stuff to uiController.js
-            // TODO: save contents from focused task item before removing focus
-            document.querySelectorAll('.task-box').forEach(item => {
-                if (item.classList.contains('focused')) {
-                    item.classList.remove('focused')
-                }
-            })
-        }
-        if (event.target.closest('li')) {
-            // check if click was on projects list item
-            // TODO: move ui stuff to uiController.js
-            // TODO: refresh view and filter by selected project (uiController.js)
-            let clickedProjectListItem = event.target.closest('li');
-            if (!clickedProjectListItem.classList.contains('selected')) {
-                document.querySelectorAll('li').forEach(item => {
-                    if (!item.classList.contains('selected')) {
-                        item.classList.remove('selected')
-                    }
-                })
-                clickedProjectListItem.classList.add('selected')
-            }
+
+        if (element == 'newTask') {
+            // TODO: create new empty task, make it focused and set pointer on title
+            // TODO: if empty task already exists, disregard clicks
+            uiController.createTask(['title test', 'text test', 'project test', 'date test']);
         }
     });
 })();
--- src/modules/uiController.js
+++ src/modules/uiController.js
@@ -1,8 +1,57 @@
-const uiController = () => {
-    const setFocus = (element, bool) => {
-        console.log('set focus to element ' + element)
+const uiController = (() => {
+    // always clear focus from task items
+    const setFocus = (event, element) => {
+        document.querySelectorAll('.task-box').forEach(item => {
+            if (item.classList.contains('focused')) {
+                item.classList.remove('focused')
+            }
+        });
+        switch (element) {
+            case 'task':
+                // add focus to clicked task
+                event.target.closest('.task-box').classList.add('focused');
+                break;
+            case 'project':
+                // add focus to clicked project
+                document.querySelectorAll('li').forEach(item => {
+                    if (!item.classList.contains('selected')) {
+                        item.classList.remove('selected')
+                    }
+                })
+                clickedProjectListItem.classList.add('selected')
+                break;
+            default:
+                break;
+        }
     };
-    return { focus };
-}
+
+    const createTask = (data) => {
+        let [title, taskText, project, date] = data;
+        const taskLocation = document.querySelector('.tasks-list');
+        const template = document.querySelector('#task-template');
+        let newTaskItem = template.content.cloneNode(true);
+
+        newTaskItem.querySelector('.task-title').textContent = title;
+        newTaskItem.querySelector('.task-text').textContent = taskText;
+        newTaskItem.querySelector('.task-project').textContent = project;
+        // TODO: date will probably be in unix format, convert to DD.MM.
+        newTaskItem.querySelector('.task-date').textContent = date;
+
+        taskLocation.append(newTaskItem);
+    };
+
+    const createProject = (data) => {
+        
+    }
+
+    const updateTaskList = () => {
+        // TODO: remove all tasks and re-add them?
+    };
+
+    return { setFocus, createTask, createProject, updateTaskList };
+})();
+
+
+
 
 export { uiController };
