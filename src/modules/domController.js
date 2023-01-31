b88b27bf53ae7c5fc4039ae8785b05eb02ebac8e
BirdCherry <58888486+BirdCherry@users.noreply.github.com>
2023-01-31 22:09:32 +0200

    Redo createTask to use web component's own function

--- src/modules/domController.js
+++ src/modules/domController.js
@@ -1,28 +1,15 @@
 const domController = (() => {
-    const createTask = (task) => {
+    const createTask = (data) => {
+        const empty = {
+            title: '',
+            text: '',
+            project: '',
+            date: Date.now(),
+        }
+
         const location = document.querySelector('.tasks-list');
         const newTask = document.createElement('task-box');
-        const taskElements = newTask.shadowRoot.querySelectorAll('.task');
-
-        for (let i = 0; i < taskElements.length; i++) {
-            let taskElementClass = taskElements[i].classList
-            switch (true) {
-                case taskElementClass.contains('title'):
-                    taskElements[i].textContent = task?.title; 
-                    break;
-                case taskElementClass.contains('text'):
-                    taskElements[i].textContent = task?.text; 
-                    break;
-                case taskElementClass.contains('project'):
-                    taskElements[i].textContent = task?.project; 
-                    break;
-                case taskElementClass.contains('date'):
-                    // taskElements[i].textContent = new Intl.DateTimeFormat().format(task?.date) ?? new Intl.DateTimeFormat().format(Date.now());
-                    taskElements[i].textContent = task?.date ?? Date.now();
-
-                    break;
-            }
-        }
+        newTask.taskData = data ?? empty;
         location.prepend(newTask);
     };
 
@@ -32,6 +19,7 @@ const domController = (() => {
 
     const updateTaskList = () => {
         // remove all tasks and re-add them?
+        // arrange tasks by date (newest first)
     };
 
     return { createTask, updateProjectList, updateTaskList };
