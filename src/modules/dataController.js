dcc08681debcd4d6e7df0b919e08a3e0a18bcb71
BirdCherry <58888486+BirdCherry@users.noreply.github.com>
2023-01-31 20:37:36 +0200

    Setup initial structure

--- src/modules/dataController.js
+++ src/modules/dataController.js
@@ -1,15 +1,23 @@
 const dataController = (() => {
-    // update data
-    // provide data
-    // save data to local storage
-    const taskList = () => {
-        if (typeof taskList === 'undefined') {
-            return taskList = [];
-        } // else if local storage exists, copy from there. else if tasklist exists and has stuff, do nothing
+    // handle local storage
+
+    const DEFAULT_DATA = [
+    { title: 'Title 1', text: 'Text 1', project: 'Project 1', date: 'Date 1', },
+    { title: 'Title 2', text: 'Text 2', project: 'Project 2', date: 'Date 2', },
+    { title: 'Title 3', text: 'Text 3', project: 'Project 3', date: 'Date 3', },
+    ];
+
+    const saveData = () => {
+        //set?
     }
     
+    const loadData = () => {
+        //get?
+        //load from local storage
+        //if not available, load default data?
+    }
 
-    return { taskList };
+    return { DEFAULT_DATA };
 })();
 
 export { dataController };
