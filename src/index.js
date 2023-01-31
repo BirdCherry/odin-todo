936f40c2214439494736f4b15281023ab986a38f
BirdCherry <58888486+BirdCherry@users.noreply.github.com>
2023-01-31 20:41:24 +0200

    Create web component, add functionality (delete, getter)

--- src/index.js
+++ src/index.js
@@ -1,48 +1,85 @@
-import css from "./style.css";
-import { uiController } from "./modules/uiController.js";
+import style from "./style.css";
+import { domController } from "./modules/domController.js";
 import { dataController } from "./modules/dataController.js";
 // TODO: add trash icon
 // TODO: add star icon (pin to top)
 
 
-const inputHandler = (() => {
-    document.addEventListener('click', event => {
-        let clickedElement = '';
-        if (event.target.closest('.task-box')) {
-            clickedElement = 'task'
-        } else if (event.target.closest('li')) {
-            clickedElement = 'project'
-        } else if (event.target.closest('.new-task')) {
-            clickedElement = 'newTask'
+// custom DOM element: task-box
+class taskboxElement extends HTMLElement {
+    constructor() {
+        super();
+        let template = document.getElementById('task-box');
+        // attach a shadow root to the element
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+        shadowRoot.appendChild(template.content.cloneNode(true));
+    }
+
+    connectedCallback() {
+        // use this to add content to element?
+        console.log('connectedCallback')
+        // TODO: look for input and save it
+        this.shadowRoot.addEventListener('input', input => {
+            console.log(input.target)
+        });
+
+        // TODO: look for click on close button and delete task
+        this.shadowRoot.querySelector('.delete').addEventListener('click',
+            click => this.deleteTask());
+
+
+        // TODO: mutationObserver? or is it really needed here?
+        // const observer = new MutationObserver(callback)
+    }
+
+    disconnectedCallback() {
+        // use this to do stuff when the element gets removed?
+        // remove event listeners? Or do they get purged automatically? I think so
+        console.log('disconnectedCallback')
+
+        this.shadowRoot.removeEventListener('input', input => {
+            console.log(input.target)
+        });
+    }
+
+    deleteTask() {
+        console.log('delete task')
+        // TODO: delete data from array as well (do this in disconnectedCallback?)
+        if (confirm("Delete this task?")) {
+            this.remove();
         }
-        uiController.setFocus(event, clickedElement);
 
-        // task item clicked: remove focus from all others, save data, set class .focused, update DOM?
+    }
 
-        // project list item clicked: set class .selected, remove class from all others, update DOM with filtered task view?
+    get taskData() {
+        // TODO: read task data and return it as an object?
+        let data = {
+            title: '',
+            text: '',
+            project: '',
+            date: '',
+        };
+        this.shadowRoot.querySelectorAll('.task').forEach(element => {
+            Object.keys(data).forEach(key => {
+                if (element.classList.contains(key)) {
+                data[key] = element.textContent;
+                }
+            })
+        })
+        return data;
+    };
 
-        // new task button clicked, create new empty task, set it at the top, set it .focused, automatically add to currently selected project?
+    set taskData(data) {
+        // TODO: insert data to correct spots
+        let {title, text, project, date} = data; // is destructuring needed?
 
-        if (clickedElement == 'newTask') {
-            // TODO: create new empty task, make it focused and set pointer on title
-            // TODO: save this to array (dataController.js), refresh task list (uiController.js)
-            let newTask = {
-                title: '',
-                text: '',
-                project: '',
-                date: Date.now(),
-            };
-            // TEMPORARY TESTING
-            uiController.createTask(newTask);
-        }
-    });
-})();
+    }
 
+}
 
+customElements.define("task-box", taskboxElement);
 
-const taskFactory = ((title, task, project, date) => {
-    const taskList = dataController.taskList;
-
-
-    return { taskList, title, task, project, date }
+const init = (() => {
+    // new task button setup
+    document.querySelector('.new-task').addEventListener('click', click => domController.createTask())
 })();
