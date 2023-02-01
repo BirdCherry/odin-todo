73d048c6fc3e3d059756915ed6ea216d87f009f7
BirdCherry <58888486+BirdCherry@users.noreply.github.com>
2023-02-01 21:52:39 +0200

    Add task save and load functionality, default tasks

--- src/modules/dataController.js
+++ src/modules/dataController.js
@@ -1,23 +1,34 @@
+import { domController } from "./domController";
+
 const dataController = (() => {
     // handle local storage
 
-    const DEFAULT_DATA = [
-    { title: 'Title 1', text: 'Text 1', project: 'Project 1', date: 'Date 1', },
-    { title: 'Title 2', text: 'Text 2', project: 'Project 2', date: 'Date 2', },
-    { title: 'Title 3', text: 'Text 3', project: 'Project 3', date: 'Date 3', },
+    const DEFAULT_TASKS = [
+        { title: 'How to edit a task', text: 'Just click on the task to edit the title, body and the project (which is set on this task as Tips).', project: 'Tips', timestamp: Date.now(), },
+        { title: 'How to delete a task', text: 'Click on the button on the bottom right of the task.', project: 'Tips', timestamp: Date.now(), },
+        { title: 'How to create a new task', text: 'Click on the yellow button on the top right of the page.', project: 'Tips', timestamp: Date.now(), },
     ];
 
-    const saveData = () => {
-        //set?
-    }
-    
-    const loadData = () => {
-        //get?
-        //load from local storage
-        //if not available, load default data?
+    const saveTasks = () => {
+        let taskArray = [];
+        document.querySelectorAll('task-box').forEach(task => {
+            taskArray.push(task.taskData)
+        })
+        // arrange by timestamp value
+        taskArray.sort((a, b) => a.timestamp - b.timestamp);
+        localStorage.setItem('tasks', JSON.stringify(taskArray));
     }
 
-    return { DEFAULT_DATA };
+    const loadTasks = () => {
+        let taskArray = localStorage.getItem('tasks')
+        // if localstorage is undefined, create introductory tasks by using default data
+        taskArray = taskArray ? JSON.parse(taskArray) : DEFAULT_TASKS;
+        taskArray.forEach(task => {
+            domController.createTask(task)
+        })
+    }
+
+    return { saveTasks, loadTasks };
 })();
 
 export { dataController };
